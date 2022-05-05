66ee6d4cdfadcafaa6bde43a85c86f829fc8f02a
Dinesh Kumar <rdk.g12@gmail.com>
2022-05-05 19:47:56 +0530

    first commit

---
+++ src/main/java/com/test/rest/sample/SpringBootAzureDeploymentApplication.java
@@ -0,0 +1,21 @@
+package com.test.rest.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAzureDeploymentApplication {
+	
+	@GetMapping("/message")
+	public String getMsg() {
+		return "Welcome to Azure Platforms";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureDeploymentApplication.class, args);
+	}
+
+}
